a0f586cb48082bde05eed309723f70da87766581
Adi0916 <michal.290305.sk@gmail.com>
2026-01-15 15:26:51 +0100

    dodanie innych headerów

---
+++ include/helpers.hxx
@@ -0,0 +1,8 @@
+//
+// Created by adi on 15.01.2026.
+//
+
+#ifndef NETSIM_HELPERS_HXX
+#define NETSIM_HELPERS_HXX
+
+#endif //NETSIM_HELPERS_HXX
---
+++ include/nodes.hxx
@@ -0,0 +1,8 @@
+//
+// Created by adi on 15.01.2026.
+//
+
+#ifndef NETSIM_NODES_HXX
+#define NETSIM_NODES_HXX
+
+#endif //NETSIM_NODES_HXX
