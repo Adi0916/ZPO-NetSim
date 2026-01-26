7a612151f87d6bf896c29228a413f95178ec0c9e
Adi0916 <michal.290305.sk@gmail.com>
2026-01-26 15:06:24 +0100

    cpp

--- src/factory.cpp
+++ src/factory.cpp
@@ -5,9 +5,12 @@
 
 /*
 is_consistent() – sprawdzanie spójności sieci
+
 do_deliveries() – dokonywanie ewentualnych dostaw na rampy
+
 do_package_passing() – dokonywanie ewentualnego
 przekazywania półproduktów
+
 do_work() – dokonywanie ewentualnego przetwarzania
 półproduktów przez robotników
 
@@ -15,3 +18,24 @@ półproduktów przez robotników
 właściwe metody z klas Ramp i Worker dla każdego elementu z
 właściwej kolekcji węzłów.)
 */
+
+
+bool Factory::is_consistent()
+{
+    return true;
+}
+
+void Factory::do_deliveries(Time t)
+{
+
+}
+
+void Factory::do_package_passing()
+{
+
+}
+
+void Factory::do_work(Time t)
+{
+
+}
